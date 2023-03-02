a016eabe7941ac10a5dc4dccff5468d519357550
Kevin Trinh <kxt.trinh@gmail.com>
2023-03-02 16:12:43 +0100

    Fixed setup

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -2,7 +2,7 @@
 require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test "visiting the homepage" do
     visit root_url # "/"
     assert_selector "h1", text: "Awesome Products"
   end
